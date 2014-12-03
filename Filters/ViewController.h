5d5455f9749faef33f1b2cc47146e48b7db667a0
saurabhj80 <saurabhj80@gmail.com>
2014-12-03 15:21:58 -0800

    Initial Commit

---
+++ Filters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Filters
+//
+//  Created by Saurabh Jain on 12/3/14.
+//  Copyright (c) 2014 Saurabh jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Filters/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Filters
+//
+//  Created by Saurabh Jain on 12/3/14.
+//  Copyright (c) 2014 Saurabh jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
